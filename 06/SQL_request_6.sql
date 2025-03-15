2043f106126dff0d284ea7310a75612c4bf35c00
Danil Gasimov <dengad2003@yandex.ru>
2025-03-15 17:47:58 +0300

    commit

--- 06/SQL_request_6.sql
+++ 06/SQL_request_6.sql
@@ -26,12 +26,40 @@ JOIN course ON soc.course_id = course.id
 GROUP BY "оценка", course.name
 ORDER BY "курс", "оценка"
 
-№2c
-
+№2c ?????????????????????????????????
+SELECT
+	ROW_NUMBER() OVER () AS "строки",
+	ROW_NUMBER() OVER (PARTITION BY course.name ORDER BY soc.course_id) AS "строки в группе",
+	course.name AS "курс",
+	CASE
+		WHEN student_rating < 30 THEN 'неудовлетворительно'
+		WHEN student_rating >= 30 AND student_rating < 60 THEN 'удовлетворительно'
+		WHEN student_rating >= 60 AND student_rating < 85 THEN 'хорошо'
+		ELSE 'отлично'
+	END as "оценка",
+	COUNT(student_id) as "количество студентов"
+FROM student_on_course AS soc
+JOIN course ON soc.course_id = course.id
+GROUP BY "оценка", course.name, soc.course_id
+ORDER BY "курс", "оценка"
 
 №2d
-
-
-№2e
-
-
+SELECT
+    DENSE_RANK() OVER (ORDER BY 
+        CASE
+            WHEN student_rating < 30 THEN 'неудовлетворительно'
+            WHEN student_rating >= 30 AND student_rating < 60 THEN 'удовлетворительно'
+            WHEN student_rating >= 60 AND student_rating < 85 THEN 'хорошо'
+            ELSE 'отлично'
+        END
+    ) AS "ранг курса и оценки", 
+    course.name AS "курс",
+    CASE
+        WHEN student_rating < 30 THEN 'неудовлетворительно'
+        WHEN student_rating >= 30 AND student_rating < 60 THEN 'удовлетворительно'
+        WHEN student_rating >= 60 AND student_rating < 85 THEN 'хорошо'
+        ELSE 'отлично'
+    END as "оценка"
+FROM student_on_course AS soc
+JOIN course ON soc.course_id = course.id
+ORDER BY "ранг курса и оценки"
