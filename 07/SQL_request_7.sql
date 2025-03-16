9c6e50acae77f4037faf346cddbe9bc54c18cb9b
Danil Gasimov <dengad2003@yandex.ru>
2025-03-16 14:09:52 +0300

    commit

--- 07/SQL_request_7.sql
+++ 07/SQL_request_7.sql
@@ -1,14 +1,20 @@
 №2a
 CREATE TABLE employees AS
 SELECT
-	ROW_NUMBER() OVER (ORDER BY student.name DESC) AS id,
+	CASE
+		WHEN student.name = 'Сергей Петров' THEN 1
+		WHEN student.name = 'Ильяс Мухаметшин' THEN 2
+		WHEN student.name = 'Иван Иванов' THEN 5
+		WHEN student.name = 'Екатерина Андреева' THEN 4
+		WHEN student.name = 'Анна Потапова' THEN 3
+	END as id
 	student.name,
 	CASE
 		WHEN student.name = 'Сергей Петров' THEN 'лидер проекта'
-		WHEN student.name = 'Ильяс Мухаметшин' THEN 'тимлид команды разработчиков'
-		WHEN student.name = 'Иван Иванов' THEN 'тимлид команды специалистов по тестированию'
+		WHEN student.name = 'Ильяс Мухаметшин' THEN 'тимлид команды разработчиков' 
+		WHEN student.name = 'Иван Иванов' THEN 'frontend разработчик'
 		WHEN student.name = 'Екатерина Андреева' THEN 'backend разработчик'
-		WHEN student.name = 'Анна Потапова' THEN 'frontend разработчик'
+		WHEN student.name = 'Анна Потапова' THEN 'тимлид команды специалистов по тестированию'
 	END as "роль",
 	CASE
 		WHEN student.name = 'Сергей Петров' THEN 1
@@ -17,44 +23,38 @@ SELECT
 		WHEN student.name = 'Екатерина Андреева' THEN 3
 		WHEN student.name = 'Анна Потапова' THEN 2
 	END as "уровень"
+	CASE
+		WHEN student.name = 'Сергей Петров' THEN [null]
+		WHEN student.name = 'Ильяс Мухаметшин' THEN 1
+		WHEN student.name = 'Иван Иванов' THEN 2
+		WHEN student.name = 'Екатерина Андреева' THEN 2
+		WHEN student.name = 'Анна Потапова' THEN 1
+	END as parent_id
 FROM student
-ORDER BY уровень
+ORDER BY уровень, роль
 
-№2b
+№2b,c Иерархический (рекурсивный) запрос
 WITH RECURSIVE temp1 AS (
-SELECT e1.name, e1.роль, CAST (e1.name AS varchar(50)) as "путь от лидера"
-FROM employees as e1 
-WHERE e1.name = 'Сергей Петров'
-UNION
-SELECT e2.name, e2.роль, cast (temp1.path || '->'|| t2.name as varchar(50))
-FROM employees as e2
-JOIN temp1 ON temp1.уровень = e2.id
+    SELECT 
+        id,
+        name AS "студент",
+        роль,
+        уровень,
+        CAST(name AS TEXT) AS "путь от лидера"
+    FROM employees 
+    WHERE parent_id IS NULL  
+    UNION ALL
+    SELECT 
+        e.id,
+        e.name,
+        e.роль,
+        e.уровень,
+        CAST(temp1."путь от лидера" || ' -> ' || e.name AS TEXT)
+    FROM employees e
+    JOIN temp1 ON e.parent_id = temp1.id
 )
-SELECT * 
+SELECT студент, роль, уровень, "путь от лидера" 
 FROM temp1
-
-
-
-WITH RECURSIVE temp1 AS (
-SELECT 
-	e1.name,
-	e1.роль,
-	e1.уровень, 
-	CAST (e1.name AS varchar(50)) as "путь от лидера"
-FROM employees as e1 
-WHERE e1.name = 'Сергей Петров'
-UNION
-SELECT 
-	e2.name,
-	e2.роль,
-	e2.уровень,
-	CAST (temp1."путь от лидера" || '->'|| e2.name as varchar(50))
-FROM employees as e2
-JOIN temp1 ON temp1.уровень + 1 = e2.уровень
-)
-SELECT *
-FROM temp1
-
-№2c
+ORDER BY уровень, id
 
 №2d
